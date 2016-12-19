febca59eafecbac6d01192c342e9a29568dcf80e
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-12-19 12:56:55 -0700

    Add registrations controller to create custom route after successful user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: "links#index"
 
   resources :links, only: [:index]
