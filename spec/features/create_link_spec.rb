84a3209d7eff51fe6e58236c5d3d1e549d572cae
Bryan Goss <bcgoss@gmail.com>
2017-01-09 13:28:48 -0700

    Test that links are drawn

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -1,9 +1,26 @@
 require "rails_helper"
 
-RSpec.describe "can create links", :js => :true do
-  scenario "Create a new link" do
-    pending 'Login must be added'
+RSpec.describe Link, :js => :true do
+  scenario 'indeas show all links for current user' do
+    me = create :user
+    link1 = create :link, title: 'Link 1', url: 'http://www.example.com/1', user: me
+    link2 = create :link, title: 'Link 2', url: 'http://www.example.com/2', user: me
+    login me
+    visit links_path
+
+    within('#links-list') do
+      expect(page).to have_text("Link 1")
+      expect(page).to have_text("http://www.example.com/1")
+      expect(page).to have_text("Link 2")
+      expect(page).to have_text("http://www.example.com/2")
+    end
+  end
+
+  scenario "can be created" do
+    me = create :user
+    login me
     visit "/"
+
     fill_in "Title:", :with => "Turing"
     fill_in "URL:", :with => "http://turing.io"
     click_on "Add Link"
@@ -12,6 +29,17 @@ RSpec.describe "can create links", :js => :true do
       expect(page).to have_text("Turing")
       expect(page).to have_text("http://turing.io")
     end
+
+  end
+
+  scenario 'can be marked read' do
+    me = create :user
+    login me
+    link = create :link, user: me
+    visit '/'
+
+save_and_open_page
+    expect { click_on 'Mark as Read' }.to change{ Link.find(link.id) }.from(false).to(true)
 
   end
 end
