6ad4e3273e980a194b26aafc367ff98bc6c06e81
Raphael Barbo <rsbarbo@gmail.com>
2017-01-09 11:56:40 -0700

    update user model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :password, presence: true
+
+  has_many :links
 end
--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -1,15 +1,28 @@
 require "rails_helper"
 
-RSpec.describe "can create links", :js => :true do
+feature "can create links" do
+  before do
+    User.create(email: "rsbarbo@example.com", password: "password")
+  end
+
   scenario "Create a new link" do
-    visit :links
-    fill_in "Title:", :with => "Turing"
+
+    visit  root_path
+
+    fill_in :email, with: "rsbarbo@example.com"
+    fill_in :password, with: "password"
+    click_button "Submit"
+
+    expect(current_path).to eq links_path
+
     fill_in "URL:", :with => "http://turing.io"
+    fill_in "Title:", :with => "Turing"
+
     click_on "Add Link"
 
     within('#links-list') do
-      expect(page).to have_text("Turing")
       expect(page).to have_text("http://turing.io")
+      expect(page).to have_text("Turing")
     end
   end
 end
