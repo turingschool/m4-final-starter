eab54b76b6eec08e37c9a32e974415514fdbd96a
Raphael Barbo <rsbarbo@gmail.com>
2017-01-09 14:39:13 -0700

    update links controller to display all the links and created the relationship in the user model

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,7 @@ class LinksController < ApplicationController
   before_action :authenticate
 
   def index
+    @links = current_user.links.all
     @hot_links = Link.hot
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :links
+
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :password, presence: true
