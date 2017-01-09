774ffcccd0ace5456b338c58bb2116b55acca632
Bryan Goss <bcgoss@gmail.com>
2017-01-09 12:15:23 -0700

    Move logged_in? to ApplicationController and mock in test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helpers 
+  helper_method :logged_in?
   def current_user
     @current_user ||= User.find_by id: session[:user_id]
   end
--- spec/requests/access_control_spec.rb
+++ spec/requests/access_control_spec.rb
@@ -1,36 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe 'Authentication:' do
+RSpec.describe SessionsController do
   context 'Guest' do
     it 'cannot view index' do
-      visit root_path
+      get root_path
 
-      expect(current_path).to eq login_path
+      expect(response).to redirect_to login_path
     end
 
     it 'can view signup page' do
-      visit signup_path
+      get signup_path
 
-      expect(current_path).to eq signup_path
+      expect(response.status).to be 200
     end
 
     it 'cannot view index' do
-      visit links_path
+      get links_path
 
-      expect(current_path).to eq login_path
+      expect(response).to redirect_to login_path
     end
   end
 
   context 'Logged in User' do
     before do
       @me = create :user
-      login @me
+      app_controller = double("ApplicationHelper")
+      allow_any_instance_of(ApplicationController).to receive(:logged_in?).and_return(true)
+
     end
 
     it 'can view index' do
-      visit links_path
-
-      expect(current_path).to eq links_path
+      get links_path
+      expect(response.status).to eq 200
     end
   end
 end
