6ea6609663ebfc8ce0e3eb5d69bca0821caa7c87
Bryan Goss <bcgoss@gmail.com>
2017-01-09 11:01:55 -0700

    Test sign up access

--- spec/features/access_control_spec.rb
+++ spec/features/access_control_spec.rb
@@ -3,6 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Authentication:' do
   context 'Guest' do
     it 'cannot view index' do
+      visit root_path
+
+      expect(current_path).to eq login_path
+    end
+
+    it 'can view signup page' do
+      visit signup_path
+
+      expect(current_path).to eq signup_path
+    end
+
+    it 'cannot view index' do
       visit links_path
 
       expect(current_path).to eq login_path
