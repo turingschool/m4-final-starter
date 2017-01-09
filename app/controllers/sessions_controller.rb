fa9343fd8aa042546df6a35e3d8a07976bed7839
Raphael Barbo <rsbarbo@gmail.com>
2017-01-09 12:18:12 -0700

    add flash notice to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to :links
     else
+      flash[:notice] = user.errors.full_messages.join(", ")
       redirect_to :login
     end
   end
