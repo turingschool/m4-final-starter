4279db16aff597c2e9070d43e6b9efd3464bbfbc
Bryan Goss <bcgoss@gmail.com>
2017-01-09 09:40:44 -0700

    Add flash messges for errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to :root
     else
+      flash[:error] = user.errors.full_messages.join('. ')
       redirect_to :signup
     end
   end
