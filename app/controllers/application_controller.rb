86a827db103b0b573fdfc380e5847fd442c8e90f
Bryan Goss <bcgoss@gmail.com>
2017-01-09 11:45:42 -0700

    Move helpers to ApplicationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helpers 
   def current_user
     @current_user ||= User.find_by id: session[:user_id]
   end
 
+  def logged_in?
+    !!current_user
+  end
+
   def require_login
-    unless current_user
+    unless logged_in?
       flash[:warn] = 'Please login first'
       redirect_to login_path
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by id: session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:warn] = 'Please login first'
+      redirect_to login_path
+    end
+  end
 end
