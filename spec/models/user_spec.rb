d729bbd3c5ce96d7aff3e035f6e28b2e414546a2
Raphael Barbo <rsbarbo@gmail.com>
2017-01-09 15:16:01 -0700

    create model test to user to make sure validations are working properly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   "add some examples to (or delete) #{__FILE__}"
+   user = User.new(email: "rsbarbo@example.com", password: "password")
+
+   # validates the presence of email and password
+   it { should validate_presence_of :email }
+   it { should validate_presence_of :password }
+
+   # => confirm that attributes are valid
+   it "expect user to be valid" do
+      expect(user).to be_valid
+   end
+
+   it "expect user to not be valid" do
+      user.email = "rsbarboexample.com"
+      expect(user).to_not be_valid
+   end
 end
