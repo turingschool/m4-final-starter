2c03066dd71d406fd0606faddcc67f51e38a1fcb
Bryan Goss <bcgoss@gmail.com>
2017-01-09 12:45:02 -0700

    Explicit url in test

--- spec/features/user_can_edit_links_spec.rb
+++ spec/features/user_can_edit_links_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe Link do
     visit edit_link_path link.id
 
     fill_in 'Title', with: 'New Title'
+    fill_in 'Url', with: 'http://www.google.com'
 
-    click_on 'Update'
+    click_on 'Update Link'
 
     expect(Link.find(link.id).title).to eq 'New Title'
   end
