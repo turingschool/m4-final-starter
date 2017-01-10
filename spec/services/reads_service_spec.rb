ebe0a544db92921201bdc0b40011ee5b3dd9c516
Bryan Goss <bcgoss@gmail.com>
2017-01-09 18:20:32 -0700

    Final service adjustment

--- spec/services/reads_service_spec.rb
+++ spec/services/reads_service_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ReadsService, type: :model do
-  it "create sends request to Hot Reads" do
-    link = create :link, read: true
-    VCR.use_cassette('send_link') do
+    it "create sends request to Hot Reads" do
+      VCR.use_cassette('send_link') do
+      link = create :link, read: true
       response = ReadsService.create(link: link)
-      byebug
       expect(response.status).to eq 201
     end
   end
