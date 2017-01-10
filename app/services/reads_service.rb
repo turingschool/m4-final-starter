c84dd4cbc11b7c510b18cdd15ffbcfde81a975d0
Bryan Goss <bcgoss@gmail.com>
2017-01-09 18:24:34 -0700

    Final adjustments to service

--- app/services/reads_service.rb
+++ app/services/reads_service.rb
@@ -1,6 +1,6 @@
 class ReadsService
-  def self.create(link)
-    new(link[:link]).send
+  def self.create(read)
+    new(read[:link]).send
   end
 
   def initialize(link)
@@ -13,14 +13,13 @@ class ReadsService
   def send
     conn.post do |req|
       req.url '/api/v1/reads'
+      req.headers['Content-Type'] = 'application/json'
       req.body = message
     end
   end
 
   def conn
-    Faraday.new(url: 'https://hotter-reads.herokuapp.com') do |config|
-      config.headers['Content-Type'] = 'application/json'
-    end
+    Faraday.new(url: 'http://localhost:3001')
   end
 
   def message
