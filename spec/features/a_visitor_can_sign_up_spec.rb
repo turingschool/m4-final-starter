c7e7c4b68f0cff9ffe5ac84177498f72b89054ce
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-04 10:07:55 -0600

    Prep visitor sign up user story.

--- spec/features/a_visitor_can_sign_up_spec.rb
+++ spec/features/a_visitor_can_sign_up_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 describe 'New user Sign Up' do
-  describe '' do
+  describe 'Through sign up form' do
+    xit 'will create account for new user and redirect to links index page' do
+      # As an unauthenticated user, if I click "Sign Up", I should be taken to a form where I can enter an email address, a password, and a password confirmation.
+      # I cannot sign up with an email address that has already been used.
+      # I cannot sign up without an email address and a password.
+      # Password and confirmation must match.
+      # If criteria is not met the user should be given a message to reflect the reason they could not sign up.
+      # Upon submitting this information, I should be logged in and redirected to the "Links Index" page.
+    end
   end
 end
