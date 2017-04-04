9b2aada22b09fa1e92b7730da397ff665433b0c8
David Knott <david1k1nott@gmail.com>
2017-04-04 10:14:34 -0600

    Create user table migration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :links, only: [:index]
   resources :login, only: [:new]
-  resources :user, only: [:new]
+  resources :users, only: [:new]
 
   namespace :api do
     namespace :v1 do
