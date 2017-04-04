60b669fa883c4e2485333df215c5f040c5648255
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-04 16:12:33 -0600

    Add user story for adding a new link into the database through API. Add new route

---
+++ spec/requests/api/v1/links_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Links API endpoints' do
+  describe 'Add a new link - POST link' do
+    it 'can add a link into the database when all details are correct' do
+      test_title = 'Test Link'
+      test_url = 'https://urlockbox-laszlo.herokuapp.com'
+      params = {
+        title: test_title,
+        url: test_url
+      }
+      post '/api/v1/links', params
+
+      expect(Link.count).to eq(1)
+      expect(Link.first.title).to eq(test_title)
+      expect(Link.first.url).to eq(test_url)
+    end
+  end
+end
