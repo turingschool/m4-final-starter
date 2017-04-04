12bc4b321acb409bfb540078d4be2b2c48139f51
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-04 12:56:05 -0600

    Pass test to go to sign up page

--- spec/features/a_visitor_is_redirected_spec.rb
+++ spec/features/a_visitor_is_redirected_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'Unauthenticated visitor' do
   describe 'Visits home page' do
     it 'will be redirected to sign in/sign up page' do
-      # As an unauthenticated user, when I visit /, I should be redirected to a page which prompts me to "Log In or Sign Up".
       visit root_path
 
       expect(page).to have_current_path(signin_path)
@@ -14,6 +13,18 @@ describe 'Unauthenticated visitor' do
       expect(page).to have_selector(:link_or_button, 'Sign In')
       expect(page).to have_selector(:link_or_button, 'Sign Up')
       expect(page).to have_link('Sign Up', :href => signup_path)
+    end
+    it 'will be redirected to sign up page if clicking on Sign Up link' do
+      visit root_path
+
+      click_on 'Sign Up'
+
+      expect(page).to have_current_path(signup_path)
+      expect(page).to have_content('Sign Up with URLockbox')
+      expect(page).to have_field('email')
+      expect(page).to have_field('password')
+      expect(page).to have_field('password_confirmation')
+      expect(page).to have_selector(:link_or_button, 'Sign Up')
     end
   end
 end
