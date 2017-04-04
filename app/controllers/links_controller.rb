c1c5ec4b02dd36ea09028cfe661f587816441168
David Knott <david1k1nott@gmail.com>
2017-04-04 11:46:04 -0600

    Create and pass a user must be logged in to access the links index page test

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,15 @@
 class LinksController < ApplicationController
+  before_action :require_login
+
   def index
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:danger] = "Please login first"
+      redirect_to root_path
+    end
   end
 end
