afdb242bff086ef8adc11b2216cfd2709c12a531
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 11:05:23 -0600

    Adds redirect after successful user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to links_path
+    else
+
+    end
   end
 
   private
