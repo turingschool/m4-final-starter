8a1b745f5878aadced1f971901ac14de2aa64dbf
David Knott <david1k1nott@gmail.com>
2017-04-04 11:36:39 -0600

    Pass user can logout from links index page test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   namespace :api do
     namespace :v1 do
