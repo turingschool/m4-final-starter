39577c2392dec84f86cb61928dd1598f5934f9cf
David Knott <david1k1nott@gmail.com>
2017-04-04 15:33:12 -0600

    Add url checking and error message displaying functionality

---
+++ app/assets/javascripts/link.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  bindCreateLinkeListenerToCreateLink();
+})
+
+function bindCreateLinkeListenerToCreateLink(){
+  $(".add-link").on("click", function(event){
+    event.preventDefault();
+    createLink();
+  })
+}
+
+function validLink(){
+  return checkUrl() && checkTitle();
+}
+
+function checkTitle(){
+  const title = $("#link_title").val();
+  return title != "";
+}
+
+function checkUrl(){
+  const url = $("#link_url").val();
+  return isUrlValid(url);
+}
+
+function isUrlValid(url) {
+  return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+}
+
+
+function getLink(){
+  return {
+    link: {
+      title: $("#link_title").val(),
+      url: $("#link_url").val()
+    }
+  };
+}
+
+function createLink(){
+  if (!validLink()) {
+    $(".message").slideDown();
+    return;
+  }
+  $(".message").slideUp();
+  $.ajax({
+    url: "/api/v1/links",
+    method: "POST",
+    data: getLink()
+  })
+  .done((newLink) => {
+    $(".links").append(newLink);
+    $("#link_title").val("");
+    $("#link_url").val("");
+  })
+  .fail((error) => {
+    console.log(error);
+  });
+}
