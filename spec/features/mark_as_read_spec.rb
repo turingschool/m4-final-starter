c50cf201aa39ad94124729a00f5144d4eee6d3d9
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 17:20:57 -0600

    Updates mark as read spec

--- spec/features/mark_as_read_spec.rb
+++ spec/features/mark_as_read_spec.rb
@@ -2,8 +2,11 @@ require "rails_helper"
 
 RSpec.describe "can mark links as read", :js => :true do
   scenario "Mark a link as read" do
-    Link.create(url:"https://turing.io", title:"Turing")
-    visit "/"
+    user = create(:user)
+    link = create(:link, user: user)
+
+    visit links_path
+
     within('.link .read-status') do
       expect(page).to have_text("false")
     end
