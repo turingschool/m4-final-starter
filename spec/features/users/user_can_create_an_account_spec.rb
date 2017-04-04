7cc66119641bac215b83b486e0b482e0fad00ea5
David Knott <david1k1nott@gmail.com>
2017-04-04 10:36:15 -0600

    Create new/edit user form view

--- spec/features/users/user_can_create_an_account_spec.rb
+++ spec/features/users/user_can_create_an_account_spec.rb
@@ -4,11 +4,11 @@ describe "When a user visits the new user page" do
   scenario "they can create an account" do
     visit new_user_path
     
-    fill_in :email, :with => "david@gmail.com"
-    fill_in :password, :with => "123"
-    fill_in :password_confirmation, :with => "123"
+    fill_in :user_email, :with => "david@gmail.com"
+    fill_in :user_password, :with => "123"
+    fill_in :user_password_confirmation, :with => "123"
 
-    click_on "Create Account"
+    click_on "Create User"
 
     expect(current_path).to eq links_path
   end
