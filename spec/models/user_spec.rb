fff7cf3666b12b067f3f413eb7f4b3165a3470d6
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 09:51:04 -0600

    Adds test for email_address validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without an email' do
+        user = User.new(password: 'password')
+
+        expect(user).to be_invalid
+      end
+    end
+  end
 end
