16d4e1c5fc81eb55e49faaf5198f1075163fef15
David Knott <david1k1nott@gmail.com>
2017-04-04 09:57:33 -0600

    Create failing guest can visit create account page test

--- spec/features/users/user_can_login_from_home_page_spec.rb
+++ spec/features/users/user_can_login_from_home_page_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 describe "When a guest visits the root path" do
-  scenario "they can click login" do
-
+  scenario "they can click login and are redirected to the login page" do
     visit root_path
 
     click_on "Login"
 
     expect(current_path).to eq new_login_path
+  end
+
+  scenario "the can click create account and are redirected to the create account page" do
+    visit root_path
+
+    click_on "Create Account"
+
+    expect(current_path).to eq new_user_path
   end
 end
