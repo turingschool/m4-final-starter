0e0b693c6650ee89e253ba1d4a7a2c630764a5d1
David Knott <david1k1nott@gmail.com>
2017-04-04 11:09:56 -0600

    Create a failing user can login test

---
+++ spec/features/sessions/user_can_login_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "When a user visits the login page" do
+  scenario "they can login if they enter valid information" do
+    user = create(:user)
+
+    visit new_login_path
+
+    fill_in :email, :with => user.email
+    fill_in :password, :with => user.password
+
+    click_on "Login"
+
+    expect(current_path).to eq links_path
+    expect(page).to have_content "You've successfully logged in!"
+  end
+end
