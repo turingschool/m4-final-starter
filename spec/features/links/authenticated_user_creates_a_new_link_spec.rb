b4ac3439a758556c8ca976c8748d61c60e0f5bd0
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 14:21:41 -0600

    Adds before each to creates a link test

--- spec/features/links/authenticated_user_creates_a_new_link_spec.rb
+++ spec/features/links/authenticated_user_creates_a_new_link_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature 'Authenticated user creates a new link' do
-  it 'they can create a link' do
+  before(:each) do
     user = create(:user)
 
     page.set_rack_session(user_id: user.id)
+  end
+  context 'successful creation' do
+    it 'they can create a link' do
+      visit links_path
 
-    visit links_path
-    
-    within('form') do
-      fill_in 'link[title]', with: 'Some Title'
-      fill_in 'link[url]', with: 'http://someurl.com'
-      click_on 'Create Link'
+      within('form') do
+        fill_in 'link[title]', with: 'Some Title'
+        fill_in 'link[url]', with: 'http://someurl.com'
+        click_on 'Create Link'
+      end
+
+      expect(current_path).to eq(links_path)
+      expect(page).to have_content('Some Title')
+      expect(page).to have_content('http://someurl.com')
     end
-
-    expect(current_path).to eq(links_path)
-    expect(page).to have_content('Some Title')
-    expect(page).to have_content('http://someurl.com')
   end
 end
