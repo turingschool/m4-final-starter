69ec522581d7d0cbe48bcf89063ac718fb289299
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 15:58:54 -0600

    Updates valid url validation to fix bug

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 class Link < ApplicationRecord
   validates :title,
             :url, presence: true
-  validates :url, url: true
+  validates :url, :url=> {:allow_nil => true}
 
   belongs_to :user
 end
