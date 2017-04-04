95d288b179d90ad8d00901ac3a61b0768b7ad01e
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 09:53:49 -0600

    Adds email_address presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :email_address, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :user do
-    
+    email_address Faker::Internet.email
   end
 end
