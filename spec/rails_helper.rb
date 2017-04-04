64b78b15cdbea306b284b7ce383d89e3c5b0f155
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 09:27:43 -0600

    Updates selenium to chrome

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,13 +6,7 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :firefox,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
-  )
-end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
