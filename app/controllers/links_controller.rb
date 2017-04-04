fc4a427bf07ff1a4b78be9f76c19d084e411693f
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 12:09:12 -0600

    Adds successful user authentication to sessions create

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,6 @@
 class LinksController < ApplicationController
   def index
     if current_user
-
     else
       redirect_to login_path
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email_address: params[:email_address])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = 'Successfully logged in!'
+      redirect_to links_path
+    end
   end
 end
