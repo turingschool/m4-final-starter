dd0638436b3b297af3c70ab8f21724ae68b15644
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 14:18:06 -0600

    Adds redirect for successful link creation

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,12 +2,25 @@ class LinksController < ApplicationController
   def index
     if current_user
       @user = current_user
-      @link = @user.links.new
+      @link = Link.new
     else
       redirect_to login_path
     end
   end
 
   def create
+    @user = current_user
+    @link = current_user.links.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
   end
 end
