425ab2f2a1ce16d92a225d7c2d4389adb2f5896b
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-04 11:20:52 -0600

    Add user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it 'has a valid model' do
-      test_user = User.new
+      test_user = User.create(email: "test@email.com", password: 'password')
 
       expect(test_user).to be_valid
+    end
+    it 'cannot have two user with the same email' do
+      test_email = 'test@email.com'
+      test_password = 'password'
+      test_user1 = User.create(email: test_email, password: test_password)
+      test_user2 = User.create(email: test_email, password: test_password)
+
+      expect(test_user1).to be_valid
+      expect(test_user2).not_to be_valid
+      expect(User.count).to eq(1)
     end
   end
 end
