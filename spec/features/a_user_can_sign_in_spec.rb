f29a301fd4b939ecfcaafd2188b2f16f126dc60b
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-04 14:59:24 -0600

    Add sad path testing for sign in for wrong email and no credentials

--- spec/features/a_user_can_sign_in_spec.rb
+++ spec/features/a_user_can_sign_in_spec.rb
@@ -49,5 +49,36 @@ describe 'Registered user Sign In' do
       expect(page).to have_content('Unsuccessful sign in. Please try again!')
     end
 
+    it 'cannot sign in without entering any credentials' do
+      test_email = 'test@email.com'
+      test_password = 'password'
+      test_user = User.create(email: test_email, password: test_password)
+
+      visit signin_path
+
+      click_on 'Sign In'
+
+      expect(page).to have_current_path(signin_path)
+      expect(page).to have_content('Unsuccessful sign in. Please try again!')
+    end
+
+    it 'cannot sign in with email of another user' do
+      test_email = 'test@email.com'
+      test_password = 'password'
+      test_user = User.create(email: test_email, password: test_password)
+      test_email2 = 'test2@email2.com'
+      test_password2 = 'password2'
+      test_user2 = User.create(email: test_email2, password: test_password2)
+
+      visit signin_path
+
+      fill_in 'user[email]', with: test_email2
+      fill_in 'user[password]', with: test_password
+      click_on 'Sign In'
+
+      expect(page).to have_current_path(signin_path)
+      expect(page).to have_content('Unsuccessful sign in. Please try again!')
+    end
+    
   end
 end
