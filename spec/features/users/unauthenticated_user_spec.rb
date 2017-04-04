e4c2650fdb56b54eb5ef3aced52b66bef1616460
Robbie Smith <robert.smith160@icloud.com>
2017-04-04 16:34:38 -0600

    Updates spacing in test

--- spec/features/users/unauthenticated_user_spec.rb
+++ spec/features/users/unauthenticated_user_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 feature 'Unauthenticated user' do
   context 'when the user visits /' do
     before { visit root_path }
+
     scenario 'the user should be redirected to the login/signup page' do
       expect(page).to have_current_path('/login')
     end
