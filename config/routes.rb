442fcb6b373107155a50af89de5e5ced24d01e69
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-04 11:12:22 -0600

    add routes to facilitate the signup process

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: "links#index"
 
-  resources :links, only: [:index]
+  get '/home',                  to: 'users#authenticate'
+  get '/signup',                to: 'users#new'
+  post '/users',                to: 'users#create'
+
+  resources :links,             only: [:index]
 
   namespace :api do
     namespace :v1 do
-      resources :links, only: [:update]
+      resources :links,         only: [:update]
     end
   end
 end
