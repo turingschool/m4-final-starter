f7af376db8569cc6274038662a636642771d19ba
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 14:47:09 -0600

    Adds test for user editing a link

---
+++ spec/features/links/authenticated_user_edits_link_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'Authenticated user edits a link' do
+  before(:each) do
+    user = create(:user)
+    @link = create(:link, title: 'Old Title', url: 'http://oldurl.com', user: user)
+    page.set_rack_session(user_id: user.id)
+    visit links_path
+  end
+
+  context 'succesful edit' do
+    it 'they can edit a link' do
+      within('table') do
+        click_on 'Edit'
+      end
+
+      expect(current_path).to eq(edit_link_path(@link))
+
+      within('form') do
+        fill_in 'link[title]', with: 'New Title'
+        fill_in 'link[url]', with: 'http://newurl.com'
+        click_on 'Submit'
+      end
+
+      expect(current_path).to eq(links_path)
+      expect(page).to have_content('New Title')
+      expect(page).to have_content('http://newurl.com')
+      expect(page).to_not have_content('Old Title')
+      expect(page).to_not have_content('http://oldurl.com')
+    end
+  end
+end
