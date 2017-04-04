ab2892f7a1aabce5fde7b764b00188deb1e956f2
David Knott <david1k1nott@gmail.com>
2017-04-04 10:27:44 -0600

    Pass user model validations tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe User do
   context "Validations" do
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_presence_of(:password_confirmation) }
   end
