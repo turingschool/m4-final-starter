e650588bb3c1c53369d6b26b1fc718d942b8a6ce
Daniel Rodriguez <drod2793@gmail.com>
2017-04-04 11:00:15 -0600

    Adds new user form

--- spec/features/unauthenticated_user_signs_up_spec.rb
+++ spec/features/unauthenticated_user_signs_up_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Unauthenticated User' do
 
       expect(current_path).to eq(new_user_path)
 
-      fill_in "user[email]", with: "d@d.com"
+      fill_in "user[email_address]", with: "d@d.com"
       fill_in "user[password]", with: "password"
       fill_in "user[password_confirmation]", with: "password"
       click_on('Create Account')
