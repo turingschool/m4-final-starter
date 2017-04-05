ebb53d7dd77395950396d60792aed62551f1da6f
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-04 19:00:34 -0600

    Refactor links controller with before_action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,13 @@
 class LinksController < ApplicationController
+  before_action :logged_in?
+
   def index
-    redirect_to signin_path if !current_user
+    @links = Link.where(user_id: current_user.id)
   end
+
+  private
+
+    def logged_in?
+      redirect_to signin_path if !current_user
+    end
 end
