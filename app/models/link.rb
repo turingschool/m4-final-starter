80aee645ea35c0499d6aed1ae54bcea30a7ca572
Laszlo Balogh <laszlo@thebaloghs.com>
2017-04-17 08:05:06 -0600

    Add link model validation for valid url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,17 +1,14 @@
 class Link < ActiveRecord::Base
   validates :user_id, presence: true
-  # before_save :valid_url?
+  validate :valid_url?
 
-  # private
+  private
 
-  #   def valid_url?
-
-  #     uri = URI.parse(self.url)
-  #     binding.pry
-  #     if !(uri.kind_of? URI::HTTP)
-  #     #  rescue URI::InvalidURIError
-  #        throw :abort
-  #       end
-  #   end
+    def valid_url?
+      uri = URI.parse(self.url)
+      if !(uri.kind_of? URI::HTTP)
+        errors.add(:url, 'Invalid')
+        end
+    end
 
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe User, type: :model do
       expect(test_link).to be_valid
     end
 
-    xit 'cannot create a link with invalid URL' do
+    it 'cannot create a link with invalid URL' do
       test_title = 'Test title'
       test_url = 'invalidURL'
       test_user_id = 1
@@ -19,6 +19,22 @@ RSpec.describe User, type: :model do
       test_link = Link.create(title: test_title, url: test_url, user_id: test_user_id)
 
       expect(test_link).not_to be_valid
+    end
+
+    it 'cannot update a link with invalid URL' do
+      test_title = 'Test title'
+      test_url = 'https://urlockbox-laszlo.herokuapp.com'
+      test_invalid_url = 'invalidURL'
+      test_user_id = 1
+
+      test_link = Link.create(title: test_title, url: test_url, user_id: test_user_id)
+      test_link.url = test_invalid_url
+      test_link.save
+
+      test_link = Link.first
+      
+      expect(test_link.url).to eq(test_url)
+      expect(test_link.url).not_to eq(test_invalid_url)
     end
 
     it 'is created with read status false' do
