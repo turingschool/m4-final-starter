439bb96c8876b8f32cec38c3b916b6ada8386be3
David Knott <david1k1nott@gmail.com>
2017-04-17 12:03:02 -0600

    Create and pass user can filter for unread links test

--- spec/features/links/user_can_filter_links_spec.rb
+++ spec/features/links/user_can_filter_links_spec.rb
@@ -3,31 +3,57 @@ require "rails_helper"
 describe "User read a link", :type => :feature, :js => true do
   it "User can filter for read links" do
     user = create(:user)
-    link = create(:link, url: "https://www.google.com", title: "Google", read: false)
-    link = create(:link, url: "https://www.google.com", title: "Google", read: false)
-    user.links << link
+    link1 = create(:link, url: "https://www.google.com", title: "Google", read: true)
+    link2 = create(:link, url: "https://www.googles.com", title: "Paul", read: false)
+    user.links << link1
+    user.links << link2
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit links_path
 
     within(".links") do
-      expect(page).to have_content link.title
-      expect(page).to have_content link.url
-      expect(page).to have_content "Read?: false"
-      expect(page).to have_link "Mark as Read"
-      expect(page).to have_link "Edit"
+      expect(page).to have_content link1.title
+      expect(page).to have_content link1.url
+      expect(page).to have_content link2.title
+      expect(page).to have_content link2.url
     end
 
-    click_link_or_button "Mark as Read"
+    click_link_or_button "Only Read Links"
 
     expect(current_path).to eq links_path
 
     within(".links") do
-      expect(page).to have_content link.title
-      expect(page).to have_content link.url
-      expect(page).to have_content "Read?: true"
-      expect(page).to have_link "Edit"
+      expect(page).to have_content link1.title
+      expect(page).to have_content link1.url
+    end
+  end
+
+  it "User can filter for unread links" do
+    user = create(:user)
+    link1 = create(:link, url: "https://www.google.com", title: "Google", read: true)
+    link2 = create(:link, url: "https://www.googles.com", title: "Paul", read: false)
+    user.links << link1
+    user.links << link2
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit links_path
+
+    within(".links") do
+      expect(page).to have_content link1.title
+      expect(page).to have_content link1.url
+      expect(page).to have_content link2.title
+      expect(page).to have_content link2.url
+    end
+
+    click_link_or_button "Only Unread Links"
+
+    expect(current_path).to eq links_path
+
+    within(".links") do
+      expect(page).to have_content link2.title
+      expect(page).to have_content link2.url
     end
   end
 end
--- spec/features/links/user_can_read_a_link_spec.rb
+++ spec/features/links/user_can_read_a_link_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "User read a link", :type => :feature, :js => true do
-  it "When a user reads an unread link read status changes to true" do
+  it "When a user clicks unread link, read status changes to true" do
     user = create(:user)
     link = create(:link, url: "https://www.google.com", title: "Google", read: false)
     user.links << link
@@ -30,31 +30,33 @@ describe "User read a link", :type => :feature, :js => true do
     end
   end
 
-  it "When a user clicks a read link read status changes to false" do
+  it "When a user clicks read link, read status changes to false" do
     user = create(:user)
-    link1 = create(:link, url: "https://www.google.com", title: "Google", read: true)
-    link2 = create(:link, url: "https://www.googles.com", title: "Paul", read: false)
-    user.links << link1
-    user.links << link2
+    link = create(:link, url: "https://www.google.com", title: "Google", read: false)
+    link = create(:link, url: "https://www.google.com", title: "Google", read: false)
+    user.links << link
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit links_path
 
     within(".links") do
-      expect(page).to have_content link1.title
-      expect(page).to have_content link1.url
-      expect(page).to have_content link2.title
-      expect(page).to have_content link2.url
+      expect(page).to have_content link.title
+      expect(page).to have_content link.url
+      expect(page).to have_content "Read?: false"
+      expect(page).to have_link "Mark as Read"
+      expect(page).to have_link "Edit"
     end
 
-    click_link_or_button "Only Read Links"
+    click_link_or_button "Mark as Read"
 
     expect(current_path).to eq links_path
 
     within(".links") do
-      expect(page).to have_content link1.title
-      expect(page).to have_content link1.url
+      expect(page).to have_content link.title
+      expect(page).to have_content link.url
+      expect(page).to have_content "Read?: true"
+      expect(page).to have_link "Edit"
     end
   end
 end
