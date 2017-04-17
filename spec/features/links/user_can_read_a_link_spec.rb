ec9dab3678f6792d7d358b5996f169cc5c6cd142
David Knott <david1k1nott@gmail.com>
2017-04-17 11:55:11 -0600

    Create and pass user can unread link test

--- spec/features/links/user_can_read_a_link_spec.rb
+++ spec/features/links/user_can_read_a_link_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "User can create a link", :type => :feature, :js => true do
-  it "When a user enters valid information a link is created" do
+describe "User read a link", :type => :feature, :js => true do
+  it "When a user reads an unread link read status changes to true" do
     user = create(:user)
     link = create(:link, url: "https://www.google.com", title: "Google", read: false)
     user.links << link
@@ -26,6 +26,35 @@ describe "User can create a link", :type => :feature, :js => true do
       expect(page).to have_content link.title
       expect(page).to have_content link.url
       expect(page).to have_content "Read?: true"
+      expect(page).to have_link "Edit"
+    end
+  end
+
+  it "When a user clicks a read link read status changes to false" do
+    user = create(:user)
+    link = create(:link, url: "https://www.google.com", title: "Google", read: true)
+    user.links << link
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit links_path
+
+    within(".links") do
+      expect(page).to have_content link.title
+      expect(page).to have_content link.url
+      expect(page).to have_content "Read?: true"
+      expect(page).to have_link "Mark as Unread"
+      expect(page).to have_link "Edit"
+    end
+
+    click_link_or_button "Mark as Unread"
+
+    expect(current_path).to eq links_path
+
+    within(".links") do
+      expect(page).to have_content link.title
+      expect(page).to have_content link.url
+      expect(page).to have_content "Read?: false"
       expect(page).to have_link "Edit"
     end
   end
