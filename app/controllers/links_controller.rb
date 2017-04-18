6cc0101e03d22b52338c916eba109676d64a1ffb
Robbie Smith <robert.smith160@icloud.com>
2017-04-18 10:15:25 -0600

    Adds functionality to edit a link and test. Still need sad path

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,4 +4,25 @@ class LinksController < ApplicationController
   def index
     @links = current_user.links.reverse
   end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      flash[:success] = "You successfully updated your link."
+      redirect_to links_path
+    else
+      # flash_message_failed_update
+      render :edit
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
 end
--- spec/features/links/edit_link_spec.rb
+++ spec/features/links/edit_link_spec.rb
@@ -2,14 +2,30 @@ require 'rails_helper'
 
 feature 'edit a link' do
   let(:user) { logged_in_user }
+  let(:link) { create(:link) }
   before do
-   user.links << create(:link)
+   user.links << link
    visit links_path
   end
 
   context 'an authenticated user edits a link' do
     scenario 'the user should see a edit button' do
       expect(page).to have_selector(:link_or_button, 'Edit')
+    end
+
+    scenario 'the user clicks on edit' do
+      click_on 'Edit'
+      expect(page).to have_current_path(edit_link_path(link))
+    end
+
+    scenario 'the user edits the link' do
+      click_on 'Edit'
+      fill_in 'link[title]', with: 'New title'
+      fill_in 'link[url]', with: 'https://www.this-is-edited.com'
+      click_on 'Submit'
+
+      expect(page).to have_content('You successfully updated your link.')
+      expect(page).to have_current_path(links_path)
     end
   end
 end
