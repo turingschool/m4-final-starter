dec6cfcbc150e51e9146e43d48fec5bd36b8844e
Robbie Smith <robert.smith160@icloud.com>
2017-04-19 15:53:51 -0600

    Adds the ability to show and hide links

--- app/assets/javascripts/hide_show.js
+++ app/assets/javascripts/hide_show.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
   $('.link-container').on('click', '.show-read', function() {
-    // event.preventDefault();
     var children = $(this).nextAll();
     children.each(function(index) {
       if ($(this).find('.link-read').text() == 'false') {
         $(this).hide();
+      } else if ($(this).find('.link-read').text() == 'true') {
+        $(this).show();
       }
     });
-    // debugger;
   });
-  // debugger;
 
+  $('.link-container').on('click', '.show-unread', function() {
+    var children = $(this).nextAll();
+    children.each(function(index) {
+      if ($(this).find('.link-read').text() == 'true') {
+        $(this).hide();
+      } else if ($(this).find('.link-read').text() == 'false') {
+        $(this).show();
+      }
+    });
+  });
 });
 
 function clearFields() {
