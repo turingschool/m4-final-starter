05b6a8fd53deb6dd768676c06487d0c6888494ec
Robbie Smith <robert.smith160@icloud.com>
2017-04-19 16:33:46 -0600

    Adds search filtering

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#search').keyup(function() {
+    $('.link-container').children('.card').filter(function(){
+      var searchValue = $('#search').val();
+      var linkText = $(this).find('.link-title').text();
+      var url = $(this).find('.link-url').text();
+      if (linkText.indexOf(searchValue) >= 0 || url.indexOf(searchValue) >= 0) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+});
