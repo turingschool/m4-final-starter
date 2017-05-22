188fe5aa344b563dccdb10f4f5f7dc39b37983de
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 09:27:23 -0600

    Adds index method to links controller
    Comments out mark_as_read_spec to make test suite quicker.
    Adds basic method to index method in links controller.
    Creates and migrates database.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,10 @@
 class LinksController < ApplicationController
 
+  def index
+    if current_user
+      @links = current_user.links
+    else
+      redirect_to 'guests#index'
+    end
+  end
 end
