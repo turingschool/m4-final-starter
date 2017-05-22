fb384a0ba13238f63cdab354aa11056afc1702c8
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-05-22 10:54:57 -0600

    make first feature test pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: "links#index"
 
-  resources :links, only: [:index]
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :links, only: [:index]
+  resources :users, only: [:new, :create]
 
   namespace :api do
     namespace :v1 do
