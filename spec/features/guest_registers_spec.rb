d42f171cfe7f25c3a742fb678bea4b289b2fa625
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 10:08:43 -0600

    Adds guest registration test for user persistence to database
    Adds test for registration form fill_out.
    Adds has_secure_password to user model.

--- spec/features/guest_registers_spec.rb
+++ spec/features/guest_registers_spec.rb
@@ -18,4 +18,18 @@ feature 'guest visits root and clicks Sign Up link' do
     expect(page).to have_field("Password")
     expect(page).to have_field("Password confirmation")
   end
+  scenario 'when guest submits completed form a user is persisted to the database' do
+    visit '/'
+
+    click_link 'Sign Up'
+
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button "Register"
+
+    new_user = User.last
+    expect(new_user.email).to eq('test@test.com')
+  end
 end
