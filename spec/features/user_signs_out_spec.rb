b7b373242ecd06e58c10e1dad057e833d6038fcc
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 12:08:57 -0600

    Adds logout-link to links index
    Test for user viewing logout link passing.

--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 feature 'logged in user' do
-
-  scenario 'sees a link to logout' do
+  before(:each) do
+    
     user = User.create(email: 'test@test.com',
                        password: 'password',
                        password_confirmation: 'password')
-    allow_any_instance_of(LinksController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit '/'
+  end
+  scenario 'sees a link to logout' do
+
+    visit '/links'
 
     expect(page).to have_link('Sign out', href: '/logout')
     expect(page).to_not have_link('Sign in', href: '/login')
