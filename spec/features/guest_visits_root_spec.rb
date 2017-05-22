63ef57dc2be76d51e5aee47512d8c6495106d3a3
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 09:37:05 -0600

    Passing assertion on guest page welcom message
    Adds guests controller with index method. Adds guests index view
    with welcome message in h1 with unique css id. Test currently
    passing.

--- spec/features/guest_visits_root_spec.rb
+++ spec/features/guest_visits_root_spec.rb
@@ -11,6 +11,8 @@ feature 'guest visits root' do
   scenario '/guests prompts guest to log-in or sign up' do
     visit '/'
 
+    expect(current_path).to eq('/guests')
+
     within('#guest-message') do
       expect(page).to have_content('Log In or Sign Up')
     end
