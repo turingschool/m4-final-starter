975614fc8a04e555a99f58807218969d6abe45fa
Max Glassie <msglassie@gmail.com>
2017-05-22 09:44:07 -0600

    basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+link_1 = Link.create(title: "what what", url: "https://www.google.com")
+link_2 = Link.create(title: "yea yea", url: "https://www.apple.com")
+link_3 = Link.create(title: "magic that", url: "https://www.getmagicnow.com")
