0b4ba2f8eb66402da61e3cd74f6eb11eadff1385
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-05-22 11:09:40 -0600

    add model test for link

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  it {is_expected.to validate_uniqueness_of(:email)}
+  context "validations" do
+    it {is_expected.to validate_uniqueness_of(:email)}
 
-  it {is_expected.to validate_presence_of(:email)}
-  it {is_expected.to validate_presence_of(:password)}
+    it {is_expected.to validate_presence_of(:email)}
+    it {is_expected.to validate_presence_of(:password)}
+  end
+
+  context "relationships" do
+    it {should have_many(:links)}
+  end
 end
