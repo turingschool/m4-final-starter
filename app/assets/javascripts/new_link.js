859bf144df1c208519b9b9cef75381ab42594c03
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-05-22 12:37:52 -0600

    add sucessful post route and ajax call

--- app/assets/javascripts/new_link.js
+++ app/assets/javascripts/new_link.js
@@ -4,5 +4,24 @@ $(document).ready(function(){
 
 function handleNewLink () {
   event.preventDefault();
-  debugger
-}
+  let form = new Object
+  form['url']   = $("input[id=link_url]").val();
+  form['title'] = $("input[id=link_title]").val();
+  addNewLink(form)
+  $.ajax({
+    url: "http://localhost:3000/api/v1/links",
+    method: "POST",
+    data: form
+  })
+  .then(function(data){
+    console.log(data)
+    debugger
+  })
+  .fail( (error) => {
+    console.error(error)
+  });
+};
+
+function addNewLink(form) {
+  
+};
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,14 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+
+  validates :title,
+            :url,
+             presence: true
+
+  def save_link(params, user)
+    self.title = params[:title]
+    self.url   = params[:url]
+    self.user  = user
+    self.save
+  end
 end
