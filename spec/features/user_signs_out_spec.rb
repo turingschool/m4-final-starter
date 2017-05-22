5a01d39fecfe2bf27201d4fc13a9f01f2ae41e51
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 12:10:58 -0600

    Add test scenario for user signing out, seeing confirmation, and being at /guests

--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'logged in user' do
   before(:each) do
-    
+
     user = User.create(email: 'test@test.com',
                        password: 'password',
                        password_confirmation: 'password')
@@ -15,5 +15,17 @@ feature 'logged in user' do
 
     expect(page).to have_link('Sign out', href: '/logout')
     expect(page).to_not have_link('Sign in', href: '/login')
+  end
+  scenario 'clicks link and is logged out' do
+
+    visit '/links'
+
+    click_link 'Sign out'
+
+    within('.alert') do
+      expect(page).to have_content('Successful sign-out. Come back soon')
+    end
+
+    expect(current_path).to eq('/guests')
   end
 end
