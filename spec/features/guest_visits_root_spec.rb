0cde395e8eb3ef4005f9ccc34cbd3b412d83cea9
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 09:31:34 -0600

    guest redirect test now green following change to expected path.

--- spec/features/guest_visits_root_spec.rb
+++ spec/features/guest_visits_root_spec.rb
@@ -5,6 +5,6 @@ feature 'guest visits root' do
   scenario 'guest is redirected to login/signup page' do
     visit '/'
 
-    expect(current_path).to eq('/guest')
+    expect(current_path).to eq('/guests')
   end
 end
