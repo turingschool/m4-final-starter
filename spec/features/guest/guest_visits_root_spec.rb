fb98a74ec5ffbcf2d074c3dbb137e5295cc39b7e
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 09:19:49 -0600

    Adds test for guest visiting root

---
+++ spec/features/guest/guest_visits_root_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+
+feature 'guest visits root' do
+  scenario 'guest is redirected to login/signup page' do
+    visit '/'
+
+    expect(current_path).to eq('/guest')
+  end
+end
