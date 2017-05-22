8ef63ebacc0a946c7336c5aa7b491cd27b432480
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-05-22 11:03:31 -0600

    add validation testing for user model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,13 @@ Capybara.register_driver :selenium do |app|
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
   )
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
