3d29c6fa8dd23000b6753ae4dca3ae7df36482c9
Andy Wooten <andrewdwooten@icloud.com>
2017-05-22 11:06:31 -0600

    Add new sessions route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :links, only: [:index]
   resources :guests, only: [:index]
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new]
   post 'users/new', to: 'users#create'
   namespace :api do
     namespace :v1 do
