b9bb9534a7eb87e1db4b4d9e3f1158d414175b1f
Edilene <edilene_cruz@yahoo.com.br>
2017-06-05 13:17:48 -0600

    Seems that adding a link works, working on loading all links with AJAX. Skipping tests for now.

--- app/assets/javascripts/addLink.js
+++ app/assets/javascripts/addLink.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
-  // addLink();
   $('#new-link').on('click', "#create-link", (event) => saveLink());
 });
 
-
-// function addLink() {
-//   $('#new-link').on('click', "#create-link", (event) => saveLink())
-// }
-
 function saveLink() {
-  debugger
   let linkData = {link: {url: $('#add-url').val(), title: $('#add-title').val() } }
   $.ajax({
     url: '/links',
@@ -17,12 +10,12 @@ function saveLink() {
     data: linkData
   }).then((data) => postLink(data))
   .fail((error)=>{
-    console.log(error)
+    console.log(error);
   });
 }
 
 function postLink(data) {
-  debugger
-  $('#url').val('')
-$('#title').val('')
-  }
+  $('#links-list .row').prepend(data);
+  $('#url').val('');
+  $('#title').val('');
+}
--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+  fetchAllLinks();
 });
+
+function fetchAllLinks(){
+  // debugger
+  $.ajax({
+    url: '/api/v1/links',
+    method: 'GET'
+}).then(alert(response))
+}
