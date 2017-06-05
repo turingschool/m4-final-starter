fb020c625f53f953c1477ef41f95448bbd2bc5a4
Andy Wooten <andrewdwooten@icloud.com>
2017-06-05 09:58:46 -0600

    Adds displayLink JS function to prepend links to div '#links'

--- app/assets/javascripts/new_link.js
+++ app/assets/javascripts/new_link.js
@@ -6,7 +6,16 @@ function createLink(){
   var url = $('input[name=url]').val()
   var title = $('input[name=title]').val()
   $.post("/api/v1/links", {link: {url: url, title: title}}).then((data) =>{
-    console.log(data)
+    if( data.url){
+      displayLink(data)
+    }
   })
+}
 
+function displayLink(link){
+  $('#links').prepend(`<div class='link'> <a href=${link.url}><h4>${link.url}</h4>
+                      </a><h4>${link.title}</h4>
+                      <form class='button_to' method='get' action="/links/${link.id}/edit">
+                      <input type="submit" value="Edit ${link.title}">
+                      </form>`)
 }
