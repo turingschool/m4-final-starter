007eeed7c6967bebf11c889ae213baeeaf5f15cc
Andy Wooten <andrewdwooten@icloud.com>
2017-06-05 14:33:50 -0600

    Add hideReadLinks and showAllLinks fx
    Adds hideReadLinks which targets and hides all read links on click of the
    'Show Unread Button'. Function is behaving as desired. Uncovered bug which
    would prevent user from alternating between show read and show unread. Added
    showAllLinks function which is invoked prior to execution of hideUnreadLinks
    and hideReadLinks functions. This enables the user to go back and forth between
    read and unread links with no need to refresh.

--- app/assets/javascripts/filter_urls.js
+++ app/assets/javascripts/filter_urls.js
@@ -1,6 +1,7 @@
 $( document ).ready(function(){
     $('input[name=filter-input]').on('keyup', filterLinks)
     $('#hide-unread-links-button').on('click', hideUnreadLinks)
+    $('#hide-read-links-button').on('click', hideReadLinks)
 })
 
 function filterLinks(){
@@ -20,8 +21,24 @@ function filterLinks(){
 }
 
 function hideUnreadLinks(){
+  showAllLinks()
   var links = $('.link.unread')
   links.each(function(index){
     $('.link.unread').eq(index).hide()
+  })
+}
+
+function hideReadLinks(){
+  showAllLinks()
+  var links = $('.link.read')
+  links.each(function(index){
+    $('.link.read').eq(index).hide()
+  })
+}
+
+function showAllLinks(){
+  var links = $('.link')
+  links.each(function(index){
+    $('.link').eq(index).show()
   })
 }
