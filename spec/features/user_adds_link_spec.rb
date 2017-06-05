55f235d42e5a5104388ca926d5c301f0b8750b4f
Andy Wooten <andrewdwooten@icloud.com>
2017-06-05 10:18:55 -0600

    Complete modification of add link tests; all tests passing.

--- spec/features/user_adds_link_spec.rb
+++ spec/features/user_adds_link_spec.rb
@@ -18,34 +18,43 @@ feature 'user visits link index', js: true do
     expect(page).to have_css("input[name=title]")
   end
 
-  scenario 'can create a link that is then associated with that user' do
+  scenario 'can create a link that is then displayed' do
 
     visit '/'
 
-    page.find('input[name=url]').set('http://www.google.com')
+    page.find('input[name=url]').set('https://www.google.com')
     page.find('input[name=title]').set('Google')
 
-    expect {click_button "Add Link"}.to change {Link.count}.by(1)
+    click_button 'Add Link'
 
+    within('.link') do
+      expect(page).to have_link('https://www.google.com', href: 'https://www.google.com')
+    end
     expect(@user.links.count).to eq(1)
   end
-  xscenario 'cannot create a link with an invalid url' do
+  scenario 'cannot create a link with an invalid url' do
 
     visit '/'
 
-    page.find('input#link_url').set('htp:/ww.yaho.com')
-    page.find('input#link_title').set('Google')
+    page.find('input[name=url]').set('htp:/ww.yaho.com')
+    page.find('input[name=title]').set('Google')
     expect {click_button "Add Link"}.to change {Link.count}.by(0)
 
+    within('.alert') do
+      expect(page).to have_content('Url is an invalid URL')
+    end
   end
   scenario 'cannot create a link without a title' do
 
     visit '/'
 
-    page.find('input#link_url').set('http://www.yahoo.com')
-    page.find('input#link_title').set('')
+    page.find('input[name=url]').set('http://www.yahoo.com')
+    page.find('input[name=title]').set('')
 
     expect {click_button "Add Link"}.to change {Link.count}.by(0)
 
+    within('.alert') do
+      expect(page).to have_content("Title can't be blank")
+    end
   end
 end
