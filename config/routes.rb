d6d5d17da6663d4b5986647778c24d4471ff3963
bschwartz10 <bschwartz92@gmail.com>
2017-07-17 16:00:30 -0600

    add edit update and create actions for links

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/login', to: 'sessions#destroy'
   resources :users, only: [:new, :create]
-  resources :links, only: [:index]
+  resources :links, only: [:index, :edit, :update, :create]
 
   namespace :api do
     namespace :v1 do
