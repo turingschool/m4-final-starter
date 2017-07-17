62f1dee24486a6d039a71e2d18341d9cd0ee84a6
Jack Pincus <jwpincus@gmail.com>
2017-07-17 15:01:41 -0600

    add sad path testing for submit new links

--- spec/features/submit_and_view_links_spec.rb
+++ spec/features/submit_and_view_links_spec.rb
@@ -14,4 +14,32 @@ RSpec.describe "Logged in user can", :js => :true do
     expect(page).to have_content('Read?: false')
     expect(page).to have_content('test link')
   end
+  scenario "submit a bad link using form will return an error message" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/'
+    expect(page).to have_current_path('/')
+    fill_in('link[url]', with: 'bad')
+    fill_in('link[title]', with: 'test link')
+    click_on('Add Link')
+    expect(page).to have_content('not a valid URL')
+  end
+
+  scenario "submit a new link using a form link persists on refresh" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    url = 'https://www.google.com'
+    visit '/'
+    expect(page).to have_current_path('/')
+    fill_in('link[url]', with: url)
+    fill_in('link[title]', with: 'test link')
+    click_on('Add Link')
+    expect(page).to have_content(url)
+    expect(page).to have_content('Read?: false')
+    expect(page).to have_content('test link')
+    visit '/'
+    expect(page).to have_content(url)
+    expect(page).to have_content('Read?: false')
+    expect(page).to have_content('test link')
+  end
 end
