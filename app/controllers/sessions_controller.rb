697e4fe141c1d2c59e3bac9c440c47e4a8793e25
glassjoseph <glassjoseph@gmail.com>
2017-07-17 13:45:27 -0600

    User session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email], password: params[:session][:email])
-    
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:sucess] = "Successfully Logged In!"
+      redirect_to links_path
+    else
+      flash.now[:failure] = "Login Unsucessful"
+      render :new
+    end
   end
 
+  def destroy
+    reset_session
+    redirect_to login_path
+  end
 end
