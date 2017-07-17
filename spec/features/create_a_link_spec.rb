758364f6f99d1d98d13c481930c6d28e1f4d3fe2
bschwartz10 <bschwartz92@gmail.com>
2017-07-17 16:15:16 -0600

    add edit and update test for links

--- spec/features/create_a_link_spec.rb
+++ spec/features/create_a_link_spec.rb
@@ -14,4 +14,22 @@ describe "Links", :type => :feature, :js => true do
     expect(page).to have_content("google")
     expect(page).to have_content("https://www.google.com/")
   end
+
+  it "go to edit form from index page and update the links title and url" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    link = create(:link, user_id: user)
+
+    visit root_path
+    click_on "Edit"
+
+    expect(current_path).to eq(edit_link_path(link))
+
+    fill_in "link[title]", :with => "yahoo"
+    fill_in "link[url]", :with => "https://www.yahoo.com/"
+    click_on "Update Link"
+    expect(Link.last.title).to eq("yahoo")
+    expect(Link.last.url).to eq("https://www.yahoo.com/")
+  end
+
 end
