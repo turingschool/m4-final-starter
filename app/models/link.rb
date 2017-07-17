2602b4b50f286af16af6d5d0b5db3806f4d14612
glassjoseph <glassjoseph@gmail.com>
2017-07-17 14:42:43 -0600

    Associate links and users
    - validate link urls

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,23 @@
+require "uri"
 class Link < ActiveRecord::Base
+belongs_to :user
+validates :title, :url, presence: true
+validate :valid_url?
+
+
+
+
+def valid_url?
+  uri = URI.parse(url)
+  unless uri.is_a?(URI::HTTP)|| uri.is_a?(URI::HTTPS) #  && !uri.host.nil?
+    errors.add(:url, "Must be a valid url")
+  end
+rescue URI::InvalidURIError
+  false
+
+end
+
+
+
 
 end
