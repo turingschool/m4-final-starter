3075c7a89f829469c64e4676798e7f6ef9ffc2cb
caward12 <ward.colleen.a@gmail.com>
2017-07-17 10:31:15 -0600

    add test for user login
    passing test
    log in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully signed in!"
+      redirect_to root_path
+    else
+      flash[:notice] = "Unsuccessful sign in attempt"
+      redirect_to new_session_path
+    end
   end
 
   def destroy
