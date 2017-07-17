45e10bd09d2963776d10d662c8e46a40a10bf68c
lukyans <sukyans@gmail.com>
2017-07-17 14:38:23 -0600

    filters by link

--- app/assets/javascripts/link.js
+++ app/assets/javascripts/link.js
@@ -1,7 +1,16 @@
-$(document).ready(function(){
-$('.search').on('keyup', searchLinks)
+function searchLinks(a) {
+  //a.preventDefault()
+
+  var filter = this.value.toUpperCase()
+  var links = document.getElementsByClassName('link-url')
+  for(var i=0; i<links.length; i++){
+    var linkName = links[i].innerHTML
+    var matchedFilter = linkName.toUpperCase().indexOf(filter)  > -1
+    links[i].parentElement.style.display = matchedFilter ? "" : "none"
+    //debugger
+  }
 }
 
-function searchLinks() {
-  
-}
+$(document).ready(function(){
+  $('.search').on('keyup', searchLinks)
+})
