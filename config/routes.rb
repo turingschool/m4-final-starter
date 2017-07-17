0ffcb4c73a5c4a19aa4f0f63a312d6e1659ba681
lukyans <sukyans@gmail.com>
2017-07-17 09:37:19 -0600

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
       resources :links, only: [:update]
     end
   end
+
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+get '/logout', to: 'sessions#destroy'
 end
