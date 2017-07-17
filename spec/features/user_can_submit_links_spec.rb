5ed7502148d536a9a3eb1d7352b54901ad4470bd
glassjoseph <glassjoseph@gmail.com>
2017-07-17 14:00:47 -0600

    test link submit

---
+++ spec/features/user_can_submit_links_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "user can submit a link" do
+  scenario "User can submit a url" do
+
+    user = User.create(email: "j@j.com", password: "password")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/"
+    within('.link_form') do
+      expect(page).to have_content("URL:")
+      expect(page).to have_content("Title:")
+      expect(page).to have_content("Add Link")
+    end
+
+    fill_in "URL:", with: "https://turing.io"
+    fill_in "Title", with: "Turing"
+    click_on "Add Link"
+    expect(page).to have_content "Title: Turing"
+    expect(page).to have_content "URL: https://turing.io"
+    expect(page).to have_content "Read?: false"
+  end
+end
