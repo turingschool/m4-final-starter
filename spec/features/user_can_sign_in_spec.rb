b7d7a14e42f2ff80764420e2d9c5cbd4a2aec13d
glassjoseph <glassjoseph@gmail.com>
2017-07-17 13:44:38 -0600

    Fix user_can_sign_in_test

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -4,30 +4,75 @@ RSpec.describe "user can sign in" do
   scenario "unauthenticated user can register" do
 
     visit "/"
-    expect(current_path).to eq("/login")
-    click_on "Register for an account"
-    expec(current_path).to eq(new_user_path)
+    click_on "Sign Up"
+    expect(current_path).to eq(new_user_path)
 
     fill_in "Email", with: "j@j.com"
     fill_in "Password", with: "password"
     fill_in "Password Confirmation", with: "password"
     click_on "Register"
 
-    expect(current_path).to eq(links_path)
+    expect(current_path).to eq('/links')
+    expect(User.count).to eq(1)
   end
 
-  # scenario "user cannot register without email" do
-  #   visit "/"
-  #   expect(current_path).to eq("/login")
-  #   expect(page).to have_content("Register for an account")
-  #   click_on "Register for an account"
-  #   expec(current_path).to eq(new_user_path)
-  #
-  #   fill_in "Email", with: "j@j.com"
-  #   fill_in "Password", with: "password"
-  #   fill_in "Password Confirmation", with: "password"
-  #   click_on "Register"
-  #
-  #   expect(current_path).to eq(links_path)
-  # end
+  scenario "user cannot register without email" do
+
+    visit "/"
+    click_on "Sign Up"
+    expect(page).to have_content("Create an Account")
+
+    fill_in "Password", with: "password"
+    fill_in "Password Confirmation", with: "password"
+    click_on "Register"
+
+    expect(current_path).to eq('/users')
+    expect(User.count).to eq(0)
+  end
+
+  scenario "user cannot register without password" do
+    visit "/"
+    click_on "Sign Up"
+    expect(current_path).to eq(new_user_path)
+
+    fill_in "Email", with: "i@i.com"
+    fill_in "Password Confirmation", with: "password"
+    click_on "Register"
+
+    expect(current_path).to eq('/users')
+    expect(User.count).to eq(0)
+  end
+
+  scenario "user cannot register without matching password and confirmation" do
+    visit "/"
+    click_on "Sign Up"
+    expect(current_path).to eq(new_user_path)
+
+    fill_in "Email", with: "i@i.com"
+    fill_in "Password", with: "cherry"
+    fill_in "Password Confirmation", with: "password"
+    click_on "Register"
+
+    expect(page).to have_content("Password and Password Confirmation must match")
+    expect(current_path).to eq('/users')
+    expect(User.count).to eq(0)
+  end
+
+  scenario "user cannot register with a taken email" do
+
+    User.create(email: "i@i.com", password: "password")
+    visit "/"
+    click_on "Sign Up"
+    expect(current_path).to eq(new_user_path)
+
+    fill_in "Email", with: "i@i.com"
+    fill_in "Password", with: "password"
+    fill_in "Password Confirmation", with: "password"
+    click_on "Register"
+    expect(page).to have_content("Email already taken")
+    expect(current_path).to eq('/users')
+    expect(User.count).to eq(1)
+  end
+
+
 end
