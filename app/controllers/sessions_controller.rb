100dfc8c4ebe85b185517a6c3c5746c6d53f0e27
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-17 13:43:42 -0600

    adds user ability to log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: session_params["email"])
+    @user = User.find_by(email: session_params['email'])
 
-    if @user && @user.authenticate(session_params["password"])
+    if @user && @user.authenticate(session_params['password'])
       session[:user_id] = @user.id
       flash[:success] = "Successfully logged in as #{@user.email}"
       redirect_to root_path
     else
-      flash[:failure] = "Login Unsuccessful"
+      flash[:failure] = 'Login Unsuccessful'
       redirect_to new_session_path
     end
+  end
+
+  def destroy
+    session.clear
+    redirect_to new_session_path
   end
 
   private
