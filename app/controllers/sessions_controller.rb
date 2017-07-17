10a401024643f3a843bac9a990ad1294527dec5f
bschwartz10 <bschwartz92@gmail.com>
2017-07-17 13:03:30 -0600

    add log in and log out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,22 @@
 class SessionsController < ApplicationController
 
-def new
+  def new; end
 
-end
+  def create
+    user = User.find_by(email: params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        flash[:success] = "Log in successful"
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        flash[:danger] = "Unsuccessful Log In"
+        redirect_to login_path
+      end
+  end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
 
 end
