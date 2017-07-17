c744457021c2090376db9cd694ad95c1569278d2
bschwartz10 <bschwartz92@gmail.com>
2017-07-17 10:44:25 -0600

    create login and new users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "links#index"
-
+  get '/login', to: 'sessions#new'
+  resources :users, only: [:new, :create]
   resources :links, only: [:index]
 
   namespace :api do
