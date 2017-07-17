1fc6058c75a2a18ac0131176adca0293219b2183
bschwartz10 <bschwartz92@gmail.com>
2017-07-17 16:49:26 -0600

    barriers for link form

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,14 +2,27 @@ $(document).ready(function(){
   $('.submit_link').on('click', function(event) {
     event.preventDefault()
     const newLink = getLinkFromForm()
-    newLink.createLink()
+    if (!newLink.title && !newLink.url) {
+      $('.error').remove()
+      $('.link_form').before('<span class="error">Please enter a title and url</span>')
+    }
+    else if (!newLink.title) {
+      $('.error').remove()
+      $('.link_form').before('<span class="error">Please enter a title</span>')
+    }
+    else if (!newLink.url) {
+      $('.error').remove()
+      $('.link_form').before('<span class="error">Please enter a url</span>')
+    }
+    if (newLink.title && newLink.url) {
+      newLink.createLink()
+    }
   })
 })
 
 function getLinkFromForm() {
     const title = $('input[name="title"]').val()
     const url = $('input[name="url"]').val()
-
 
     return new Link ({
       title: title,
