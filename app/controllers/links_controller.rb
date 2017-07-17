fa08d1b2ad1bcf75b02bc46c9903689a054f34ef
glassjoseph <glassjoseph@gmail.com>
2017-07-17 17:57:32 -0600

    Add edit and update to Links Controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -19,10 +19,17 @@ class LinksController < ApplicationController
     else
       render json: @link.errors.full_messages.to_s.delete('"'), status: 500
     end
-
-
   end
 
+  def edit
+    @link  = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update(link_params)
+    redirect_to links_path
+  end
 
   def link_params
     params.require(:link).permit(:title, :url)
