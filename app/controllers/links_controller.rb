945bb6d73bfcb520fddb14c04935f6be4bc6c0ab
lukyans <sukyans@gmail.com>
2017-07-17 11:00:07 -0600

    add link form

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,26 @@
 class LinksController < ApplicationController
 
+  def index
+    if current_user
+      @links = Link.where(user: current_user)
+      @add_link = Link.new
+      binding.pry
+    end
+  end
+
+  def create
+    link = Link.new(link_params)
+    link.user = current_user
+    if link.url
+      link.save
+      redirect_to links_path
+    else
+      flash[:error] = "Link is not valid"
+      redirect_to link_path
+    end
+end
+
+    def link_params
+      params.require(:link).permit(:url, :title, :read, :user)
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,13 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'shoulda/matchers'
 
+Shoulda::Matchers.configure do |config|
+config.integrate do |with|
+with.test_framework :rspec
+with.library :rails
+end
+end
+
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(
     app,
