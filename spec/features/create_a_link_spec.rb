9f9cf84e9dc7ba795eb316bfd0f0e097c6d683bc
bschwartz10 <bschwartz92@gmail.com>
2017-07-17 19:34:48 -0600

    manually create link b/c of data threading

--- spec/features/create_a_link_spec.rb
+++ spec/features/create_a_link_spec.rb
@@ -18,12 +18,14 @@ describe "Links", :type => :feature, :js => true do
   it "go to edit form from index page and update the links title and url" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    link = create(:link, user_id: user)
-
     visit root_path
-    click_on "Edit"
 
-    expect(current_path).to eq(edit_link_path(link))
+    fill_in "title", :with => "google"
+    fill_in "url", :with => "https://www.google.com/"
+    click_link_or_button "Add Link"
+
+    click_on "Edit"
+    expect(current_path).to eq(edit_link_path(Link.last))
 
     fill_in "link[title]", :with => "yahoo"
     fill_in "link[url]", :with => "https://www.yahoo.com/"
