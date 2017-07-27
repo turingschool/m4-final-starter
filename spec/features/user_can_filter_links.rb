788b4899093414e61f5239026b6f038256583e95
glassjoseph <glassjoseph@gmail.com>
2017-07-27 11:33:42 -0600

    Test search-filter

--- spec/features/user_can_filter_links.rb
+++ spec/features/user_can_filter_links.rb
@@ -38,7 +38,7 @@ RSpec.describe "User can filter links", :js => :true do
     expect(page).to have_text("Title: Turing")
     expect(page).to have_text("URL: https://pizza.io")
     expect(page).to have_text("Title: Pizza")
-
+  
     click_on "Only Unread Links"
 
     expect(page).to_not have_text("URL: https://pizza.io")
@@ -49,7 +49,7 @@ RSpec.describe "User can filter links", :js => :true do
       expect(page).to have_text("Title: Turing")
     end
   end
-  
+
   scenario "User shows all links" do
     user = User.create(email: "j@j.com", password: "password")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -75,5 +75,36 @@ RSpec.describe "User can filter links", :js => :true do
       expect(page).to have_text("URL: https://turing.io")
       expect(page).to have_text("Title: Turing")
     end
+  end
+
+  scenario "User can filter with search bar" do
+    user = User.create(email: "j@j.com", password: "password")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    user.links.create(url:"https://turing.io", title:"Turing")
+    user.links.create(url:"https://Pizza.io", title:"Pizza")
+    visit "/"
+
+    fill_in "search-filter", with: "Turing"
+
+    expect(page).to_not have_text("URL: https://Pizza.io")
+    expect(page).to_not have_text("Title: Pizza")
+
+    within('.link') do
+      expect(page).to have_text("URL: https://turing.io")
+      expect(page).to have_text("Title: Turing")
+    end
+
+    fill_in "search-filter", with: "pizza"
+
+    expect(page).to_not have_text("URL: https://turing.io")
+    expect(page).to_not have_text("Title: Turing")
+
+    within('.link') do
+      expect(page).to have_text("URL: https://Pizza.io")
+      expect(page).to have_text("Title: Pizza")
+    end
+
+
   end
 end
