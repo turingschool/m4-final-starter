f1af99de9462ac25064918dbc477be329d1eb800
glassjoseph <glassjoseph@gmail.com>
2017-07-27 09:38:14 -0600

    Add to mark_as_read_spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-
   def check_login
     redirect_to login_path unless current_user
   end
--- spec/features/mark_as_read_spec.rb
+++ spec/features/mark_as_read_spec.rb
@@ -1,8 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "can mark links as read", :js => :true do
-  xscenario "Mark a link as read" do
-    Link.create(url:"https://turing.io", title:"Turing")
+  scenario "Mark a link as read" do
+    user = User.create(email: "j@j.com", password: "password")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    user.links.create(url:"https://turing.io", title:"Turing")
     visit "/"
     within('.link .read-status') do
       expect(page).to have_text("false")
@@ -12,6 +15,24 @@ RSpec.describe "can mark links as read", :js => :true do
 
     within('.link .read-status') do
       expect(page).to have_text("true")
+    end
+
+  end
+
+  scenario "Mark a link as unread" do
+    user = User.create(email: "j@j.com", password: "password")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    user.links.create(url:"https://turing.io", title:"Turing", read: true)
+    visit "/"
+    within('.link .read-status') do
+      expect(page).to have_text("true")
+    end
+
+    click_on "Mark as Unread"
+
+    within('.link .read-status') do
+      expect(page).to have_text("false")
     end
 
   end
